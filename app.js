730f87b1858d201fef35652dd6b1419b0dbd4b8e
nhathuy009 <47845818+nhathuy009@users.noreply.github.com>
2026-08-10 22:40:48 +0700

    Hotfix: temporary restore app.js via previous commit CDN until full file push

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// RESTORE_PENDING - will replace
+/* Temporary bootstrap: load last known-good app.js from commit 5b7c8e2 */
+(function(){
+  var s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/nhathuy009/sepay-order-lookup@5b7c8e2a4b28a4a9387deeaf7ac5b646a26c062f/app.js';
+  s.async = false;
+  document.head.appendChild(s);
+})();
