258ae2b96720c95065a14e81adad01275144802f
nhathuy009 <47845818+nhathuy009@users.noreply.github.com>
2026-09-09 15:52:10 +0700

    Fix typo in type field from 'MOVIDE' to 'MOVIE'

--- 123avupdate-new.js
+++ 123avupdate-new.js
@@ -22,7 +22,7 @@ function getManifest() {
         "iconUrl": "https://123av.com/assets/123av/favicon.png",
         "isEnabled": true,
         "isAdult": true,
-        "type": "MOVIDE",
+        "type": "MOVIE",
         "layoutType": "HORIZONTAL",
         "playerType": "exoplayer",
         "subtitleCat": true,
